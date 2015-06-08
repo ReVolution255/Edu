6c60edc246aabf8a64c684fad0522be0c5c31473
ReVolution255 <grivoos@yandex.com>
2015-06-08 17:13:07 +0300

    No comments

--- src/ru/resolutionpoint/edu/animals/model/Constants.java
+++ src/ru/resolutionpoint/edu/animals/model/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     private static int predatorLifeTime = 10000;
     private static int noBreedingAnimalSteps = 20;
     private static int noBreedingPredatorSteps = 3000;
-    private static int neighboringAnimalsLimit = 5;
+    private static int neighboringAnimalsLimit = 3;
     private static int predatorSatiationTime = 1000;
     private static int predatorTime = 500;
     private static int TIME_DELAY = 50;
